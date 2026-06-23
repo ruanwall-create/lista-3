c8be110231c8ebb816529c69fb5235b9e4398fc0
Ruan <ruan_wall@estudante.sesisenai.org.br>
2026-06-22 21:11:10 -0300

    questao 01

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Atividade1();
+
+static void Atividade1()
+{
+    double[] notas = new double[5];
+    double soma = 0;
+    double media;
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        Console.Write("Informe a nota: ");
+        notas[i] = Convert.ToDouble(Console.ReadLine());
+
+        soma += notas[i];
+    }
+
+    media = soma / notas.Length;
+
+    Console.WriteLine($"\nSoma das notas: {soma}");
+    Console.WriteLine($"Média final: {media}");
+}
