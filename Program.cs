f4cc778232be422590e00ccf867e91610421f8b7
Ruan <ruan_wall@estudante.sesisenai.org.br>
2026-06-22 22:18:04 -0300

    questao 10

--- Program.cs
+++ Program.cs
@@ -1,47 +1,35 @@
-﻿Atividade9();
+﻿Atividade10();
 
-static void Atividade9()
+static void Atividade10()
 {
-    string[] tarefas = new string[5];
-    int[] prioridades = new int[5];
+    string[] estudantes = new string[8];
 
-    int baixa = 0;
-    int media = 0;
-    int alta = 0;
-
-    for (int i = 0; i < tarefas.Length; i++)
+    for (int i = 0; i < estudantes.Length; i++)
     {
-        Console.Write("Informe o nome da tarefa: ");
-        tarefas[i] = Console.ReadLine()!;
+        Console.Write("Informe o nome do estudante: ");
+        estudantes[i] = Console.ReadLine()!;
+    }
 
-        Console.Write("Informe a prioridade da tarefa: ");
-        prioridades[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Informe o nome para busca: ");
+    string busca = Console.ReadLine()!;
 
-        if (prioridades[i] == 1)
+    int posicaoEncontrada = -1;
+
+    for (int i = 0; i < estudantes.Length; i++)
+    {
+        if (estudantes[i] == busca)
         {
-            baixa++;
-        }
-        else if (prioridades[i] == 2)
-        {
-            media++;
-        }
-        else if (prioridades[i] == 3)
-        {
-            alta++;
+            posicaoEncontrada = i + 1; // posição começa em 1
+            break;
         }
     }
 
-    Console.WriteLine($"\nTarefas de baixa prioridade: {baixa}");
-    Console.WriteLine($"Tarefas de média prioridade: {media}");
-    Console.WriteLine($"Tarefas de alta prioridade: {alta}");
-
-    Console.WriteLine("\nTarefas de alta prioridade:");
-
-    for (int i = 0; i < tarefas.Length; i++)
+    if (posicaoEncontrada != -1)
     {
-        if (prioridades[i] == 3)
-        {
-            Console.WriteLine($"- {tarefas[i]}");
-        }
+        Console.WriteLine($"\nEstudante encontrado na posição {posicaoEncontrada}.");
+    }
+    else
+    {
+        Console.WriteLine("\nEstudante não encontrado.");
     }
 }
